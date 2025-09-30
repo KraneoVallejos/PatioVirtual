a58e0e42c3bb6f735c51f4c9da13707e0abbaca7
Ariel <ariel.vallejos.m@gmail.com>
2025-09-30 20:17:58 -0300

    Agrega a módulo API archivo login.php

---
+++ backend/api/login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+include "conexion.php";
+
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recuperar datos del formulario y evitar inyecciones SQL con real_escape_string
+    $correo = $conexion->real_escape_string($_POST["correo"]);
+    $contrasena = $_POST["contrasena"]; 
+
+    // Consulta SQL preparada para verificar credenciales y evitar inyecciones SQL
+
+    $sql = "SELECT id_usuario, contrasena, nombre FROM usuario WHERE correo=?";
+    $consulta = $conexion->prepare($sql);
+    $consulta->bind_param('s', $correo);
+    $consulta->execute();
+    $consulta->store_result();
+
+    // si la verificación resulta correcta se redirige a página de bienvenida
+
+	if ($consulta->num_rows == 1) {
+        $consulta->bind_result($id_usuario, $hashed_password, $nombre);
+        $consulta->fetch();
+
+        //verificar hash
+        if(password_verify($contrasena, $hashed_password)){
+            session_regenerate_id(true);
+            $_SESSION["id_usuario"] = $id_usuario;
+            $_SESSION["usuario"] = $nombre;
+            $_SESSION["correo"] = $correo;
+            header("Location: iniciocorrecto.php");
+        	exit;
+        } else {
+        header("Location: index.php?error=1");
+        }
+    }else{
+        header("Location: index.php?error=1");
+    }
+    $consulta->close();
+}
+$conexion->close();
+?>
