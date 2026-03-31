e2a5839307c462df849407929dab6dc43795121f
Ariel Vallejos <ariel.vallejos.m@gmail.com>
2026-03-31 18:42:47 -0300

    Corrección de directorio en script login.php

--- backend/api/login.php
+++ backend/api/login.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-include "../backend/db/conexion.php";
+include "../db/conexion.php";
 
 // Verificar si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recuperar datos del formulario y evitar inyecciones SQL con real_escape_string
     $correo = $conexion->real_escape_string($_POST["correo"]);
-    $contrasena = $_POST["contrasena"]; 
+    $contrasena = $_POST["contrasena"];
 
     // Consulta SQL preparada para verificar credenciales y evitar inyecciones SQL
 
@@ -18,25 +18,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // si la verificación resulta correcta se redirige a página de bienvenida
 
-	if ($consulta->num_rows == 1) {
+    if ($consulta->num_rows == 1) {
         $consulta->bind_result($id_usuario, $hashed_password, $nombre);
         $consulta->fetch();
 
         //verificar hash
-        if(password_verify($contrasena, $hashed_password)){
+        if (password_verify($contrasena, $hashed_password)) {
             session_regenerate_id(true);
             $_SESSION["id_usuario"] = $id_usuario;
             $_SESSION["usuario"] = $nombre;
             $_SESSION["correo"] = $correo;
             header("Location: ../../frontend/iniciocorrecto.php");
-        	exit;
+            exit;
         } else {
-        header("Location: ../../frontend/index.php?error=1");
+            header("Location: ../../frontend/index.php?error=1");
         }
-    }else{
+    } else {
         header("Location: ../../frontend/index.php?error=1");
     }
     $consulta->close();
 }
 $conexion->close();
-?>
