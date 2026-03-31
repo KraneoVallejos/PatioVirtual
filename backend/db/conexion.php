1c2072924ca0ae43b64ff42a698568509e484817
Ariel Vallejos <ariel.vallejos.m@gmail.com>
2026-03-31 20:40:13 -0300

    Modifica valores de parametros "USUARIO" Y "PASSWORD" para conexión local

--- backend/db/conexion.php
+++ backend/db/conexion.php
@@ -1,8 +1,8 @@
 <?php
-define("SERVIDOR","localhost"); //Servidor de base de datos (por defecto localhost)
-define("USUARIO","usuario"); // Usuario de la base de datos
-define("PASSWORD","contraseña"); //Contraseña del usuario
-define("BD","patiovirtual");
+define("SERVIDOR", "localhost"); //Servidor de base de datos (por defecto localhost)
+define("USUARIO", "root"); // Usuario de la base de datos
+define("PASSWORD", ""); //Contraseña del usuario
+define("BD", "patiovirtual");
 
 //generar conexión
 $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BD);
@@ -12,4 +12,3 @@ if ($conexion->connect_error) {
     die("Error de conexión a la base de datos: " . $conexion->connect_error);
 }
 $conexion->set_charset("utf8mb4"); // Se establece el tipo de codificación a UTF-8
-?>
