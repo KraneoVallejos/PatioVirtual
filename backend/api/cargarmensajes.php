c9e9d901f3132ea5612ba099ee09c5bb5617bd04
Ariel Vallejos <ariel.vallejos.m@gmail.com>
2026-04-09 12:14:57 -0400

    Modifica: - Se desplaza el llamado a conexion.php posterior a la validación de usuario. - se agrega códigos http 401 para indicar error por acceso no autorizado - se agrega código 501 para indicar error desde servidor -los mensajes se ordenan por id - se agrgea cierre de consulta y liberación de memoria

--- backend/api/cargarmensajes.php
+++ backend/api/cargarmensajes.php
@@ -1,33 +1,43 @@
 <?php
 
 session_start();
-require_once "../db/conexion.php";
 
 //Cabecera JSON
 header('Content-Type: application/json; charset=utf-8');
 
 // Verificación de inicio de sesión
-if (!isset($_SESSION["usuario"])) {
-    echo json_encode(["success" => false, "error" => "acceso denegado"]);
-    exit;
+if (!isset($_SESSION["id_usuario"])) {
+    http_response_code(401);
+    echo json_encode([
+        "success" => false,
+        "error" => "acceso denegado"
+    ], JSON_UNESCAPED_UNICODE);
+    exit();
 }
+
+require_once "../db/conexion.php";
 
 // consulta SQL con JOIN 
 $sql2 = "SELECT m.fecha, u1.nombre AS remitente, m.mensaje, m.estado
 FROM mensajes m
 JOIN usuario u1 ON m.remitente_id = u1.id_usuario
-ORDER BY m.fecha ASC";
+ORDER BY m.id_mensajes ASC";
 $resultado = $conexion->query($sql2);
 
-if (!$resultado){
-    echo json_encode(["success" => false, "error" => "error en la consulta". $conexion->error]);
-    exit;
+if (!$resultado) {
+    http_response_code(500);
+    echo json_encode([
+        "success" => false,
+        "error" => "error en la consulta: " . $conexion->error
+    ], JSON_UNESCAPED_UNICODE);
+    $conexion->close();
+    exit();
 }
 
 $mensajes = [];
 
 if ($resultado->num_rows > 0) {
-    while ($fila = $resultado->fetch_assoc()){
+    while ($fila = $resultado->fetch_assoc()) {
         $mensajes[] = [
             "fecha" => $fila["fecha"],
             "remitente" => $fila["remitente"],
@@ -38,5 +48,12 @@ if ($resultado->num_rows > 0) {
     }
 }
 
-echo json_encode(["success" => true, "mensajes" => $mensajes], JSON_UNESCAPED_UNICODE);
-exit;
+// Libera de memoria el resultado de consulta SQL y cierra la conexión
+$resultado->free();
+$conexion->close();
+
+echo json_encode([
+    "success" => true,
+    "mensajes" => $mensajes
+], JSON_UNESCAPED_UNICODE);
+exit();
