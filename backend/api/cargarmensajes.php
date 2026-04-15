7ad697dd0c89bec81297d5bb3123f9e33ae9791f
Ariel Vallejos <ariel.vallejos.m@gmail.com>
2026-04-15 18:20:37 -0400

    Modifica: - Se agrega try/catch para asegurar el correcto procesamiento de errores no contemplados - se elimina "if" innecesario para asociar resultados de la consulta con parámetros respectivos - catch procesa errores de conexión posteriores al inicio de sesión

--- backend/api/cargarmensajes.php
+++ backend/api/cargarmensajes.php
@@ -5,38 +5,38 @@ session_start();
 //Cabecera JSON
 header('Content-Type: application/json; charset=utf-8');
 
-// Verificación de inicio de sesión
-if (!isset($_SESSION["id_usuario"])) {
-    http_response_code(401);
-    echo json_encode([
-        "success" => false,
-        "error" => "acceso denegado"
-    ], JSON_UNESCAPED_UNICODE);
-    exit();
-}
+try {
+        // Verificación de inicio de sesión
+    if (!isset($_SESSION["id_usuario"])) {
+        http_response_code(401);
+        echo json_encode([
+            "success" => false,
+            "error" => "acceso denegado"
+        ], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
 
-require_once "../db/conexion.php";
+    require_once "../db/conexion.php";
 
-// consulta SQL con JOIN 
-$sql2 = "SELECT m.fecha, u1.nombre AS remitente, m.mensaje, m.estado
-FROM mensajes m
-JOIN usuario u1 ON m.remitente_id = u1.id_usuario
-ORDER BY m.id_mensajes ASC";
-$resultado = $conexion->query($sql2);
+    // consulta SQL con JOIN 
+    $sql2 = "SELECT m.fecha, u1.nombre AS remitente, m.mensaje, m.estado
+    FROM mensajes m
+    JOIN usuario u1 ON m.remitente_id = u1.id_usuario
+    ORDER BY m.id_mensajes ASC";
+    $resultado = $conexion->query($sql2);
 
-if (!$resultado) {
-    http_response_code(500);
-    echo json_encode([
-        "success" => false,
-        "error" => "error en la consulta: " . $conexion->error
-    ], JSON_UNESCAPED_UNICODE);
-    $conexion->close();
-    exit();
-}
+    if (!$resultado) {
+        http_response_code(500);
+        echo json_encode([
+            "success" => false,
+            "error" => "error en la consulta: " . $conexion->error
+        ], JSON_UNESCAPED_UNICODE);
+        $conexion->close();
+        exit();
+    }
 
-$mensajes = [];
+    $mensajes = [];
 
-if ($resultado->num_rows > 0) {
     while ($fila = $resultado->fetch_assoc()) {
         $mensajes[] = [
             "fecha" => $fila["fecha"],
@@ -46,14 +46,22 @@ if ($resultado->num_rows > 0) {
 
         ];
     }
+
+    // Libera de memoria el resultado de consulta SQL y cierra la conexión
+    $resultado->free();
+    $conexion->close();
+
+    echo json_encode([
+        "success" => true,
+        "mensajes" => $mensajes
+    ], JSON_UNESCAPED_UNICODE);
+    exit();
+
+} catch (mysqli_sql_exception $e) {
+    http_response_code(500);
+    echo json_encode([
+        "success" => false,
+        "error" => "error en la conexión: " 
+    ], JSON_UNESCAPED_UNICODE);
+    exit();
 }
-
-// Libera de memoria el resultado de consulta SQL y cierra la conexión
-$resultado->free();
-$conexion->close();
-
-echo json_encode([
-    "success" => true,
-    "mensajes" => $mensajes
-], JSON_UNESCAPED_UNICODE);
-exit();
