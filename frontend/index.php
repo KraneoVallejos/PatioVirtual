defef142aa15e300e5190aaf48d3d3257db888ae
Ariel Vallejos <ariel.vallejos.m@gmail.com>
2026-04-21 18:37:12 -0400

    Modifica index.php: - migración a método AJAX para consultas asincrónicas - envío de datos mediante FormData - procesamiento de respuestas JSON desde nueva versión de login.php - mejora manejo de errores

--- frontend/index.php
+++ frontend/index.php
@@ -2,7 +2,7 @@
 <html lang="es">
 
 <head>
-	<meta charset=utf-8>
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="estilos.css" />
 	<title>Iniciar sesión</title>
@@ -11,12 +11,12 @@
 <body>
 	<h2>Bienvenido! inicia sesión</h2>
 	<div>
-		<form action="../backend/api/login.php" method="post">
+		<form id="formCredencial">
 			<label for="correo">ingresa correo institucional</label><br>
-			<input type="email" id="correo" name="correo" required>
+			<input type="email" id="correo" name="correo" maxlength="50" required>
 			<br>
 			<label for="contrasena">contraseña</label><br>
-			<input type="password" id="contrasena" name="contrasena" required>
+			<input type="password" id="contrasena" name="contrasena" maxlength="30" required>
 			<br>
 			<br>
 			<button type="submit">INICIO SESIÓN</button>
@@ -25,10 +25,39 @@
 
 	<div>
 		<h2>Crear nuevo usuario</h2>
-		<form action="crearUsuario.php" method="$_POST">
+		<form action="crearUsuario.php" method="get">
 			<button type="submit">crear nuevo registro</button>
 		</form>
 	</div>
+	<script>
+		async function loginUsuario(e) {
+			e.preventDefault();
+
+			try {
+				const formCredencial = e.target;
+				const formdata = new FormData(formCredencial);
+				const res = await fetch("../backend/api/login.php", {
+					method: "POST",
+					body: formdata
+				});
+
+				const data = await res.json();
+
+				if (!data.success) {
+					alert("error: " + data.error);
+					return;
+				}
+
+				window.location.href= "iniciocorrecto.php";
+
+			} catch (error) {
+				console.error(error);
+				alert("error en la conexión con el servidor");
+			}
+		}
+		document.getElementById("formCredencial").addEventListener("submit", loginUsuario);
+
+	</script>
 </body>
 
 </html>
