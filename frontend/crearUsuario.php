c9f61726e12333dfd0fa4768195db5c7365b1bcf
Ariel Vallejos <ariel.vallejos.m@gmail.com>
2026-04-26 18:06:01 -0400

    Agrega a crearUsuario.php : - cuadro de texto para mostrar información sobre resultados de solicitudes del usuario (info_estado) - función que muestra estado de la solicitud desde la API - función que limpia contenedor de mensajes informativos

--- frontend/crearUsuario.php
+++ frontend/crearUsuario.php
@@ -16,56 +16,72 @@ require_once "../backend/db/conexion.php";
 
 	<h1>BIENVENIDO!!</h1>
 	<h2>Regístrate</h2>
+	<p> Ingresa tu nombre completo, tu correo institucional y una contraseña</p>
+	<p id="info_estado"></p>
 
 	<div>
-	<p> Ingresa tu nombre completo, tu correo institucional y una contraseña</p>
-	<form id="formRegistro" method="post">
+		
+		<form id="formRegistro" method="post">
 
-		<label for="nombre">nombre</label><br>
-		<input type="text" id="nombre" name="nombre" placeholder = "nombre... " required maxlength="30">
-		<br>
-		<br>
-		<label for="apellpat">primer apellido</label><br>
-		<input type="text" id="apellpat" name="apellpat" placeholder = "primer apellido... " required maxlength="30">
-		<br>
-		<br>
-		<label for="apellmat">segundo apellido</label><br>
-		<input type="text" id="apellmat" name="apellmat" placeholder = "segundo apellido... " required maxlength="30">
-		<br>
-		<br>
-		<label for="correo">correo electrónico</label><br>
-		<input type="email" id="correo" name="correo"  placeholder = "nombre@email.com..." required maxlength="50">
-		<br>
-		<br>
-		<label for="carrera">Selecciona carrera</label><br><br>
-        <select id="carrera" name="carrera" required>
-            <option value="">--Seleccione--</option>
-            <?php
-            $res = $conexion->query("SELECT nombre_carrera FROM carreras");
-            while ($fila = $res->fetch_assoc()) {
-                echo "<option value='" . htmlspecialchars($fila['nombre_carrera'], ENT_QUOTES, 'UTF-8') . "'>"
-				. htmlspecialchars($fila['nombre_carrera'], ENT_QUOTES, 'UTF-8') 
-				. "</option>";
-            }
-			$res->free();
-			$conexion->close();
-            ?>
-        </select><br><br>
+			<label for="nombre">nombre</label><br>
+			<input type="text" id="nombre" name="nombre" placeholder = "nombre... " required maxlength="30">
+			<br>
+			<br>
+			<label for="apellpat">primer apellido</label><br>
+			<input type="text" id="apellpat" name="apellpat" placeholder = "primer apellido... " required maxlength="30">
+			<br>
+			<br>
+			<label for="apellmat">segundo apellido</label><br>
+			<input type="text" id="apellmat" name="apellmat" placeholder = "segundo apellido... " required maxlength="30">
+			<br>
+			<br>
+			<label for="correo">correo electrónico</label><br>
+			<input type="email" id="correo" name="correo"  placeholder = "nombre@email.com..." required maxlength="50">
+			<br>
+			<br>
+			<label for="carrera">Selecciona carrera</label><br>
+			<select id="carrera" name="carrera" required>
+				<option value="">--Seleccione--</option>
+				<?php
+				$res = $conexion->query("SELECT nombre_carrera FROM carreras");
+				while ($fila = $res->fetch_assoc()) {
+					echo "<option value='" . htmlspecialchars($fila['nombre_carrera'], ENT_QUOTES, 'UTF-8') . "'>"
+					. htmlspecialchars($fila['nombre_carrera'], ENT_QUOTES, 'UTF-8') 
+					. "</option>";
+				}
+				$res->free();
+				$conexion->close();
+				?>
+			</select><br><br>
 
-		<label for="contrasena">crea una contraseña</label><br>
-		<input type="password" id="contrasena" name="contrasena" autocomplete="off" required maxlength="30">
-		<br>
-		<p> Verifica tus datos y presiona confirmar</p>
-		<button type="submit">confirmar</button>
-	</form>
-
-	<form action="index.php" method="get">
-				<p> Si ya tienes cuenta puedes volver a la página de inicio</p>
-		<button type="submit">volver al inicio</button>
-	</form>
+			<label for="contrasena">crea una contraseña</label><br>
+			<input type="password" id="contrasena" name="contrasena" autocomplete="off" required maxlength="30">
+			<br>
+			<p> Verifica tus datos y presiona confirmar</p>
+			<button type="submit">confirmar</button>
+		</form>
 	</div>
+
+		<div>
+			<form action="index.php" method="get">
+				<p>Si ya tienes cuenta puedes volver a la página de inicio</p>
+				<button type="submit">volver al inicio</button>
+			</form>
+		</div>
+
 <!-- Consulta con método AJAX para enviar y almacenar datos de nuevo usuario en BD -->
 	<script>
+
+		const infoEstado = document.getElementById("info_estado");
+
+		function mostrarInfo(mensaje) {
+			infoEstado.innerHTML = `<p>${mensaje}</p>`; 
+		}
+
+		function limpiarInfo() {
+			infoEstado.innerHTML = "";
+		}
+
 		async function registrarUsuario(e) {
 			e.preventDefault();
 
@@ -80,16 +96,19 @@ require_once "../backend/db/conexion.php";
 				const data = await res.json();
 
 				if (!data.success) {
-					alert("error: " + data.error);
+					mostrarInfo(data.error);
+					setTimeout(limpiarInfo,3000);
 					return;
 				}
 
-				alert("usuario registrado con éxito");
+				mostrarInfo(data.mensaje);
+				setTimeout(limpiarInfo,5000);
 				formRegistro.reset();
 				window.location.href=("index.php")
 								
 			} catch (error) {
-				alert("Error en el envío de datos: " + error.message);	
+				mostrarInfo(`Error en el envío de datos: ${error.message}`);
+				setTimeout(limpiarInfo,3000);
 			}
 		}
 		document.getElementById("formRegistro").addEventListener("submit", registrarUsuario);
